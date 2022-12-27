79798c24ccb91f0702498e4b85d6f77ad6452d37
jafar <jafar.fahimi12@gmail.com>
2022-12-27 23:59:08 +0430

    isotope increased

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,30 @@
 //   layoutMode: 'fitRows'
 // });
 
-
 // init Isotope
-var $grid = $('.collection-list').isotope({
-    // options
+var $grid = $(".collection-list").isotope({
+  // options
+});
+// filter items on button click
+$(".filter-button-group").on("click", "button", function () {
+  var filterValue = $(this).attr("data-filter");
+  resetFilterBtns();
+  $(this).addClass("active-filter-btn");
+  $grid.isotope({ filter: filterValue });
+});
+var filterBtns = $(".filter-button-group").find("button");
+function resetFilterBtns() {
+  filterBtns.each(function () {
+    $(this).removeClass("active-filter-btn");
   });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
+}
+
+// // init Isotope
+// var $grid = $('.collection-list').isotope({
+//   // options
+// });
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+//   $grid.isotope({ filter: filterValue });
+// });
